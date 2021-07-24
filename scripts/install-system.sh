364e3cd96a673393f62fee5c8054be3f405650e0
syntaxbullet <84109220+syntaxbullet@users.noreply.github.com>
2021-07-24 21:00:02 +0200

    added screenlayout default to install script, fixed files without newlines

--- scripts/install-system.sh
+++ scripts/install-system.sh
@@ -9,5 +9,6 @@ cp ../dotfiles/.picom /home/${USER}/.config/picom/picom.conf
 cp ../dotfiles/.i3-config /home/${USER}/.config/i3/config
 # set git configuration
 cp ../dotfiles/.gitconfig /home/${USER}/.gitconfig
-# echo "Install complete" in a rainbow color    
+# set xrandr screenlayout 
+cp ./xrandr-screenlayouts/default.sh /home/${USER}/.screenlayout/default.sh   
 echo -e "\e[1;31mInstall complete\e[0m"
